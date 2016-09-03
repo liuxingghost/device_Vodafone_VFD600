3fb800229dc328f244a9de0b31cd2b8b3c1ac491
Wzedlare <vedatak01@gmail.com>
2016-09-03 09:53:22 +0300

    Fix qcom-radio

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -4,7 +4,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.custom_ecc=1 \
     persist.radio.ecc_hard_1=998 \
     persist.radio.ecc_hard_count=1 \
-    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
     ro.telephony.default_network=9,9
 
 # RIL
